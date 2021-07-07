31b12e0d150bd4b0e349156ee36da4099cefc202
eightgran <eightgran@gmail.com>
2021-07-07 20:34:06 +0200

    Add `UserDataController`

---
+++ lib/controller/user_data_controller.dart
@@ -0,0 +1,21 @@
+import 'package:remaining_lifetime/model/user_data.dart';
+
+/// A controller to implement analyses and features based on [UserData].
+class UserDataController {
+  /// The data on which the analyses are performed.
+  final UserData userData;
+
+  /// Creates a [UserDataController].
+  const UserDataController(this.userData);
+
+  /// Returns the user's age.
+  int get age {
+    DateTime now = DateTime.now();
+    DateTime birth = DateTime.fromMillisecondsSinceEpoch(userData.dayOfBirth!);
+    int ageTimestamp =
+        now.millisecondsSinceEpoch - birth.millisecondsSinceEpoch;
+    int ageYears = Duration(milliseconds: ageTimestamp).inDays ~/ 365;
+    print(DateTime.fromMillisecondsSinceEpoch(ageYears));
+    return ageYears;
+  }
+}
