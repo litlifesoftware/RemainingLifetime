b6e2b492cd79533939d999202355706eb4384384
eightgran <eightgran@gmail.com>
2021-07-08 19:30:22 +0200

    Add `DefaultUserData`

---
+++ lib/data/default_user_data.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+/// Stores default [UserData] member values in case some data is missing on the
+/// Database.
+class DefaultUserData {
+  static const Color defaultColor = Color(0xFFEED6D6);
+}
