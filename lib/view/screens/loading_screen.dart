93608f965b3bfc8ae76229ea18039d3e227103c2
eightgran <eightgran@gmail.com>
2021-07-15 22:21:26 +0200

    Redesign `LoadingScreen`

--- lib/view/screens/loading_screen.dart
+++ lib/view/screens/loading_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lit_ui_kit/lit_ui_kit.dart';
 
-/// A screen [Widget] displaying placeholder content while the actual content
-/// is either being loaded or background task are executed which are essential
-/// for the upcoming screen.
+/// A screen widgets displayed as long as data is being fetched.
 class LoadingScreen extends StatelessWidget {
-  /// Creates a [LoadingScreen] [Widget].
+  /// Creates a [LoadingScreen].
   const LoadingScreen({
     Key? key,
   }) : super(key: key);
@@ -13,27 +11,54 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Container(
-          height: 50.0,
-          width: 50.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25.0),
-            color: Colors.white24,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: const [
+              LitColors.mediumGrey,
+              LitColors.darkBlue,
+            ],
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Text(
-                '!',
-                style: LitTextStyles.sansSerif.copyWith(
-                  color: Colors.black38,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
+        ),
+        child: Center(
+          child: _InfoIcon(),
+        ),
+      ),
+    );
+  }
+}
+
+class _InfoIcon extends StatelessWidget {
+  const _InfoIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 64.0,
+      width: 64.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24.0),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(-2, 2),
+            blurRadius: 4.0,
+            color: Colors.black26,
+            spreadRadius: -1.0,
+          )
+        ],
+        color: LitColors.mediumGrey,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Icon(
+            LitIcons.info,
+            size: 24.0,
+            color: LitColors.lightGrey.withOpacity(0.3),
           ),
         ),
       ),
