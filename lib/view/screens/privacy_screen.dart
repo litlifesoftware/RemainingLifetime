bb97151fe018f71b3b63b56412a4bd4e3cb7b3b9
eightgran <eightgran@gmail.com>
2021-07-15 22:22:21 +0200

    Add documentation on `PrivacyScreen`

--- lib/view/screens/privacy_screen.dart
+++ lib/view/screens/privacy_screen.dart
@@ -3,11 +3,22 @@ import 'package:lit_ui_kit/lit_ui_kit.dart';
 import 'package:lit_ui_kit/screens.dart';
 import 'package:remaining_lifetime/controller/localization/remaining_lifetime_localizations.dart';
 
+/// A screen widgets implementing the [LitPrivacyDisclaimerScreen] to inform
+/// the user about the app's privacy.
+///
+/// The [onConfirm] defaults to navigate back, once the 'confirm' button is
+/// pressed.
 class PrivacyScreen extends StatefulWidget {
+  /// Creates a [PrivacyScreen].
   const PrivacyScreen({
     Key? key,
     this.onConfirm,
   }) : super(key: key);
+
+  /// Handles the action once the 'confirm' button is pressed.
+  /// This will either be used to:
+  /// - navigate back (default, if none value was provided)
+  /// - call a custom function (e.g. to initialize the database)
   final void Function()? onConfirm;
 
   @override
@@ -15,6 +26,7 @@ class PrivacyScreen extends StatefulWidget {
 }
 
 class _PrivacyScreenState extends State<PrivacyScreen> {
+  /// Navigates back to the previous screen.
   void _navigateBack() {
     LitRouteController(context).navigateBack();
   }
