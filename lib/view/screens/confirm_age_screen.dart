7b0c3a23c600d4f2635ebb3778eb9ccaf565955f
eightgran <eightgran@gmail.com>
2021-07-16 22:33:10 +0200

    Add documentation on several screens.

--- lib/view/screens/confirm_age_screen.dart
+++ lib/view/screens/confirm_age_screen.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:lit_ui_kit/lit_ui_kit.dart';
 import 'package:remaining_lifetime/controller/localization/remaining_lifetime_localizations.dart';
 
+/// A screen widget allowing the user to submit his age.
+///
+/// The age will be used to verify whether the user passes the minimum age
+/// requried to use this app and to create the initial user data.
 class ConfirmAgeScreen extends StatelessWidget {
+  /// A callback excuted once the 'onSubmit' button is pressed.
   final void Function(DateTime age) onSubmit;
+
+  /// Creates a [ConfirmAgeScreen].
   const ConfirmAgeScreen({
     Key? key,
     required this.onSubmit,
--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,26 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:lit_ui_kit/lit_ui_kit.dart';
 import 'package:remaining_lifetime/controller/lifetime_controller.dart';
 import 'package:remaining_lifetime/view/screens/lifetime_screen.dart';
 import 'package:remaining_lifetime/view/screens/profile_screen.dart';
-import 'package:remaining_lifetime/view/widgets/bottom_bar.dart';
 
-/// The home screen which will be display once the database has been created.
-/// Its main purpose is to display the basic elements like the [BottomBar].
-/// Furthermore it will contain [Widget]s to interact with and display data
-/// provided by the [appSettingsBox] and [goalsBox].
+/// A screen widget to allow navigation between the [LifetimeScreen] and
+/// [ProfileScreen].
 class HomeScreen extends StatefulWidget {
-  final Box<dynamic> appSettingsBox;
-  final Box<dynamic> goalsBox;
   final LifetimeController? lifetimeController;
 
   /// Creates a [HomeScreen] [Widget].
 
   const HomeScreen({
     Key? key,
-    required this.appSettingsBox,
-    required this.goalsBox,
     required this.lifetimeController,
   }) : super(key: key);
   @override
@@ -28,8 +20,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  /// States whether to hide the bottom navigation bar (in case the settings
+  /// should be displayed instead panel).
   bool hideNavigationBar = false;
 
+  /// The first screen's tab data.
+  static const LitBottomNavigationTabData _tabDataScreen1 =
+      const LitBottomNavigationTabData(
+    index: 0,
+    icon: LitIcons.home_alt,
+    iconSelected: LitIcons.home,
+  );
+
+  /// The second screen's tab data.
+  static const LitBottomNavigationTabData _tabDataScreen2 =
+      const LitBottomNavigationTabData(
+    index: 1,
+    icon: LitIcons.person,
+    iconSelected: LitIcons.person_solid,
+  );
+
+  /// Toggles the [hideNavigationBar] state value.
   void toggleHideNavigationBar() {
     setState(() {
       hideNavigationBar = !hideNavigationBar;
@@ -53,22 +64,17 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
       hideNavigationBar: hideNavigationBar,
       tabs: [
         LitNavigableTab(
-          tabData: LitBottomNavigationTabData(
-              index: 0, icon: LitIcons.home_alt, iconSelected: LitIcons.home),
+          tabData: _tabDataScreen1,
           screen: LifetimeScreen(
-            appSettingsBox: widget.appSettingsBox,
-            goalsBox: widget.goalsBox,
             lifetimeController: widget.lifetimeController,
             toggleHideNavigationBar: toggleHideNavigationBar,
           ),
         ),
         LitNavigableTab(
-          tabData: LitBottomNavigationTabData(
-              index: 1,
-              icon: LitIcons.person,
-              iconSelected: LitIcons.person_solid),
-          screen:
-              ProfileScreen(toggleHideNavigationBar: toggleHideNavigationBar),
+          tabData: _tabDataScreen2,
+          screen: ProfileScreen(
+            toggleHideNavigationBar: toggleHideNavigationBar,
+          ),
         ),
       ],
     );
