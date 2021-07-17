55febda9ddd03e45a498c08d8fc886dbab62e32a
eightgran <eightgran@gmail.com>
2021-07-17 22:03:31 +0200

    Add documentation on `AboutAppDialog`

--- lib/view/widgets/about_dialog.dart
+++ lib/view/widgets/about_dialog.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:lit_ui_kit/dialogs.dart';
 import 'package:remaining_lifetime/view/widgets/launcher_image.dart';
 
+/// A dialog widget displaying informations about the `Remaining Lifetime`
+/// app.
+///
+/// It uses the [LitAboutDialog].
 class AboutAppDialog extends StatelessWidget {
+  /// Creates a [AboutAppDialog].
   const AboutAppDialog({Key? key}) : super(key: key);
 
   @override
